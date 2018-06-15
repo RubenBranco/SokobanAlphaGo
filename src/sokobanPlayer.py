9637ebf31dd1cc5af1f2e3031053c72b72925be8
Ruben Branco <ruben.branco@outlook.pt>
2018-06-15 23:56:51 +0100

    Standard player complete(greedy). Lowercase letter start correction

--- src/sokobanPlayer.py
+++ src/sokobanPlayer.py
@@ -1,6 +1,18 @@
-class sokobanPlayer():
+class SokobanPlayer():
     def __init__(self, game):
         self.game = game
 
     def play(self, board):
-        pass
+        valids = board.get_valid_moves()
+        candidates = []
+
+        for action in range(self.game.get_action_size()):
+            if valids[action] == 0:
+                continue
+
+            next_state = self.game.get_next_state(board, action)
+            score = self.game.get_score(next_state)
+            candidates.append((-score, action))
+        candidates.sort()
+        
+        return candidates[0][1]
